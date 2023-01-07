bb0878ad09aaaee6d42ba0a7c8b4849dc5a2071a
Cr0wn_Gh0ul <troy.salem.work@gmail.com>
2023-01-07 11:45:15 -0500

    more progress

--- contracts/AuctionRuntime.sol
+++ contracts/AuctionRuntime.sol
@@ -6,8 +6,8 @@ import "./RedBlackTreeLibrary.sol";
 
 contract AuctionRuntime {
     using RedBlackTreeLibrary for RedBlackTreeLibrary.Tree;
+
     RedBlackTreeLibrary.Tree bidTree;
-    
     struct AuctionData {
         uint256 maxWinningBids;
         uint256 winningBidsPlaced;
@@ -19,96 +19,119 @@ contract AuctionRuntime {
         uint256 totalPrizes;
     }
     AuctionData public auction;
+
     mapping(address => uint256) addressToAmount;
     mapping(uint256 => address) amountToAddress;
 
-    constructor(AuctionData calldata auction_) {
-        auction = auction_;
+    address private _auctionOwner;
+
+    modifier onlyAuctionOwner() {
+        require(
+            msg.sender == _auctionOwner,
+            "Only auction owner can call this function"
+        );
+        _;
     }
-
-    function destroyAuction() private {
-        selfdestruct(msg.sender);
-    }
-
-
-            /**
+    /**
      * @dev checks if the auction is active and okay for bidding or bid removal
-     * TODO: ONLY ENDED is needed
      */
     modifier isAuctionActive() {
-        require(block.number <= auction_.auctionBase.endTime, "Auction has ended");
+        require(block.number <= auction.endTime, "Auction has ended");
         _;
     }
 
-    function addBid() public isAuctionActive {
-        validateBid(msg.sender, msg.value);
+    modifier hasActiveBid() {
+        require(addressToAmount[msg.sender] > 0, "No bid exists");
+        _;
+    }
+
+    constructor(AuctionData memory auction_, address auctionOwner_) {
+        auction = auction_;
+        _auctionOwner = auctionOwner_;
+    }
+
+    function destroyAuction() private {
+        selfdestruct(payable(msg.sender));
+    }
+
+    function addBid() public payable isAuctionActive {
+        AuctionData memory auction_ = auction;
+        bool isReplacement = _validateBid(auction_, msg.sender, msg.value);
         // if number of winning bids is equal to max number of winning bids
-        if (lowestBid > 0) {
-            // remove lowest winning bid from bid tree
-            auction_.bidTree.remove(bidTree.first());
-            // send lowest winning bid back to bidder
-            _refundBid(
-                auction_,
-                auction_.amountToAddress[lowestBid],
-                lowestBid
-            );
+        if (isReplacement) {
+            uint256 lowestBid = bidTree.first();
+            _removeBid(amountToAddress[lowestBid], lowestBid);
         } else {
             // increment number of winning bids
-            auction_.auctionBase.winningBidsPlaced++;
+            auction_.winningBidsPlaced++;
         }
         // add bid to active bid mapping
-        auction_.addressToAmount[msg.sender] = msg.value;
+        addressToAmount[msg.sender] = msg.value;
         // for reverse tree lookup
-        auction_.amountToAddress[msg.value] = msg.sender;
+        amountToAddress[msg.value] = msg.sender;
         // add bid to bid tree
-        auction_.bidTree.insert(msg.value);
-        auction_.auctionBase.totalValue += msg.value;
+        bidTree.insert(msg.value);
+        auction_.totalValue += msg.value;
+        auction = auction_;
     }
 
-    function removeBid(Auction storage auction_) external checkActiveAuction(auction_) {
-        // get bid on this auction
-        uint256 lastBid = auction_.addressToAmount[msg.sender];
-        auction_.addressToAmount[msg.sender] = 0;
-        // remove lowest winning bid from bid tree
-        auction_.bidTree.remove(lastBid);
-        // send lowest winning bid back to bidder
-        // delete bid from active winningBidsPlaced
-        auction_.auctionBase.winningBidsPlaced--;
-        auction_.auctionBase.totalValue -= lastBid;
-        _refundBid(auction_, auction_.amountToAddress[lastBid], lastBid);
+    function removeBid() external isAuctionActive hasActiveBid {
+        auction.winningBidsPlaced--;
+        _removeBid(msg.sender, addressToAmount[msg.sender]);
+
+    }
+    function _removeBid(address user_, uint256 amount_) internal {
+        delete addressToAmount[user_];
+        delete amountToAddress[amount_];
+        bidTree.remove(amount_);
+        bool success = _refundBid(user_, amount_);
+        if (success) {
+            auction.totalValue -= amount_;
+        }
     }
 
-    function increaseBid(Auction storage auction_) external checkActiveAuction(auction_) {
-        require(auction_.addressToAmount[msg.sender] > 0, "No bid exists");
-        // get bid on this auction
-        uint256 lastBid = auction_.addressToAmount[msg.sender];
+    function increaseBid() external payable isAuctionActive hasActiveBid {
+        AuctionData memory auction_ = auction;
+        uint256 lastBid = addressToAmount[msg.sender];
         uint256 newBid = (lastBid + msg.value);
-        // check if new bid is greater than last
         require(
-            newBid > lastBid + ((lastBid * auction_.auctionBase.minBidIncrement) / 100),
+            newBid >
+                lastBid +
+                    ((lastBid * auction_.minBidIncrement) / 100),
             "New bid is lower than last bid plus minimum bid increment"
         );
-        auction_.bidTree.remove(lastBid);
-        auction_.auctionBase.totalValue += msg.value;
-        auction_.addressToAmount[msg.sender] = newBid;
-        // for reverse tree lookup
-        auction_.amountToAddress[newBid] = msg.sender;
-        // add bid to bid tree
-        auction_.bidTree.insert(newBid);
+        bidTree.remove(lastBid);
+        auction_.totalValue += msg.value;
+        addressToAmount[msg.sender] = newBid;
+        amountToAddress[newBid] = msg.sender;
+        bidTree.insert(newBid);
+        auction = auction_;
     }
 
-    function validateBid(address address_, uint256 amount_) public view returns (bool isReplacement) {
+    function validateBid(address address_, uint256 amount_)
+        public
+        view
+        returns (bool isReplacement)
+    {
         AuctionData memory a = auction;
-        precheckBid(a, address_, amount_);
-        return isReplacementBid(a, amount_);
+        return _validateBid(a, address_, amount_);
     }
+
+    function _validateBid(AuctionData memory auction_, address address_, uint256 amount_)
+        internal
+        view
+        returns (bool isReplacement)
+    {
+        preCheckBid(auction_, address_, amount_);
+        return isReplacementBid(auction_, amount_);
+    }
+
 
     function preCheckBid(
         AuctionData memory auction_,
-        uint256 amount_,
-        address addr_
-    ) internal view isAuctionActive 
-    returns (uint256) {
+        address addr_,
+        uint256 amount_
+    ) internal view isAuctionActive {
         // check if bid already exists
         require(addressToAmount[addr_] == 0, "Bidder already bid");
         // chheck if bid is greater than reserve price
@@ -118,14 +141,12 @@ contract AuctionRuntime {
         );
         // check if bid for this amount already exists
         require(!bidTree.exists(amount_), "Bid amount already exists");
-        // check if new bid is valid
-        return newValidBid(auction_, amount_);
     }
 
     function isReplacementBid(AuctionData memory auction_, uint256 value_)
         internal
         view
-        returns (uint256)
+        returns (bool)
     {
         // see if we are replacing a bid
         if (auction_.winningBidsPlaced >= auction_.maxWinningBids) {
@@ -147,20 +168,23 @@ contract AuctionRuntime {
     /**
      * @dev Returns the winners of an auction
      */
-    function _winnersOfAuction(Auction storage auction_)
+    function _winnersOfAuction()
         internal
         view
         returns (address[] memory)
     {
+        AuctionData memory auction_ = auction;
         // create an array of addresses to store winners
-        address[] memory winners = new address[](auction_.auctionBase.winningBidsPlaced);
+        address[] memory winners = new address[](
+            auction_.winningBidsPlaced
+        );
         // get the first bid in the bid tree
-        uint256 currentValue = auction_.bidTree.first();
-        for (uint256 i = 0; i < auction_.auctionBase.winningBidsPlaced; i++) {
+        uint256 currentValue = bidTree.first();
+        for (uint256 i = 0; i < auction_.winningBidsPlaced; i++) {
             // add the address of the bid from the bid tree to the winners array
-            winners[i] = auction_.amountToAddress[currentValue];
+            winners[i] = amountToAddress[currentValue];
             // get the next bid in the bid tree
-            currentValue = auction_.bidTree.next(currentValue);
+            currentValue = bidTree.next(currentValue);
         }
         // return the winners array
         return winners;
@@ -169,19 +193,9 @@ contract AuctionRuntime {
     /**
      * @dev Refunds a bid and removes the bid from the active bid mapping
      */
-    function _refundBid(
-        Auction memory auction_,
-        address user_,
-        uint256 amount_
-    ) internal {
-        // delete bid from active bid mapping
-        delete auction_.amountToAddress[amount_];
+    function _refundBid(address to_, uint256 amount_) internal returns (bool) {
         // refund bid
-        (bool success, ) = user_.call{gas: 30000, value: amount_}("");
-        if (!success) {
-            auction_.totalValue += amount_;
-        }
-        auction = auction_;
-        // if the user was a contract doing some weird stuff then the refund goes to auction revenue
+        (bool success, ) = to_.call{gas: 30000, value: amount_}("");
+        return success;
     }
 }
